d7f18e5d0ba7abea2d16fc781c1b3428e09a7454
Lifechoices <mpendulokhozamk2@gmail.com>
2021-05-12 16:42:21 +0200

    first commit

---
+++ slides.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    margin: 50,
+    loop: true,
+    stagePadding: 100,
+    dots: false,
+    autoplay: true,
+  });
+});
