7c56532f85490bb054e05e22822c655b863e2ef4
lifechoices <mpendulokhozamk22gmail.com>
2021-07-21 15:27:12 +0200

    preloader

---
+++ javascript/pre-loader.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+  "use strict";
+  setTimeout(function () {
+    $(".loader_bg").fadeToggle();
+  }, 1500);
+HTML:  <div class="loader_bg"><div class="loader"></div></div>
+})
